7346086f3beeed0d50853c8d342f17fd46108b28
MariaKolyda <iis20004@uom.edu.gr>
2022-12-06 18:46:03 +0200

    updated dependencies

--- src/main/java/maria/main.java
+++ src/main/java/maria/main.java
@@ -1,10 +1,32 @@
 package maria;
 
+import java.io.File;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("maria");
+		try {
+			Git.cloneRepository()
+			.setURI("https://github.com/eclipse/jgit.git")
+			.setDirectory(new File("/Desktop")) 
+			.call();
+		} catch (InvalidRemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransportException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (GitAPIException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
